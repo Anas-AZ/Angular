7c4f41d5ec375bcb21d3e36a65aaa21f19203f2b
Anas <anas9991az@gmail.com>
2023-04-20 15:06:34 +0530

    Controlling Navigation with canDeactivate

--- 11.Changing pages with Routing/routing-start/routing-start/src/app/app-routing.module.ts
+++ 11.Changing pages with Routing/routing-start/routing-start/src/app/app-routing.module.ts
@@ -8,15 +8,16 @@ import { ServerComponent } from "./servers/server/server.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { AuthGuard } from "./authGuard.service";
+import { CanDeactivateGuard } from "./canDeactivateGuard.service";
 
 const appRoutes : Routes = [
   {path : '', component : HomeComponent},
   {path : 'users', component : UsersComponent, children: [
     {path : ':id/:name', component : UserComponent}
   ]},
-  {path : 'servers',canActivate: [AuthGuard], component : ServersComponent, children : [
+  {path : 'servers',canActivateChild: [AuthGuard], component : ServersComponent, children : [
     {path : ':id', component : ServerComponent},
-    {path : ':id/edit', component : EditServerComponent}
+    {path : ':id/edit', component : EditServerComponent, canDeactivate: [CanDeactivateGuard] }
   ]},
   {path: 'not-found', component: PageNotFoundComponent},
   {path: '**', redirectTo: '/not-found'}
