e367945cc5668ad64c458482cb1736621411681e
Anas <anas9991az@gmail.com>
2023-04-26 17:31:05 +0530

    added operators

--- 13.Understanding Observables/obs-01-start/obs-01-start/src/app/home/home.component.ts
+++ 13.Understanding Observables/obs-01-start/obs-01-start/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable, Subscription, interval } from 'rxjs';
-import { timeout } from 'rxjs-compat/operator/timeout';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -35,7 +35,18 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000);
     });
 
-    this.firstObsSubscription =  customIntervalObs.subscribe(data => {
+    const transformedData = customIntervalObs.pipe(filter(
+      (data: number) => {
+        return data > 0
+      }
+    )
+     , map(
+       (data: number) => {
+        return 'Round '+( data + 1);
+      }
+    ));
+
+    this.firstObsSubscription =  transformedData.subscribe(data => {
       console.log(data);
     }, error => {
       console.log(error);
