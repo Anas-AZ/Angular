77123fe4ef6eec548296a013445d0eea22ec40dd
Anas <anas9991az@gmail.com>
2023-04-26 11:54:21 +0530

    observables

---
+++ 13.Understanding Observables/obs-01-start/obs-01-start/src/app/home/home.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription, interval } from 'rxjs';
+import { timeout } from 'rxjs-compat/operator/timeout';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObsSubscription: Subscription;
+
+  constructor() { }
+
+  ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe(
+    //    {next: count=>{
+    //     console.log(count);
+    //    }
+    //   }
+    // );
+
+    const customIntervalObs = new Observable(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if(count === 2){{
+          observer.complete();
+        }}
+        if(count>3){
+          observer.error(new Error('count is greater than 3'));
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.firstObsSubscription =  customIntervalObs.subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    }, ()=>{
+      console.log('completed');
+    });
+
+  }
+
+  ngOnDestroy(): void {
+      this.firstObsSubscription.unsubscribe();
+  }
+
+}
