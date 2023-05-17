fc0e3fd74fc89ad1335b08c0d7fdba14c126fa50
Anas <anas9991az@gmail.com>
2023-05-17 09:56:55 +0530

    sending a POST request

--- 18.Making Http Requests/http-01-start/http-01-start/src/app/app.component.ts
+++ 18.Making Http Requests/http-01-start/http-01-start/src/app/app.component.ts
@@ -15,6 +15,10 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
+    this.http.post('https://ng-complete-guide-b0c8d-default-rtdb.firebaseio.com/posts.json', postData)
+    .subscribe((responseData)=>{
+      console.log(responseData);
+    });
     console.log(postData);
   }
 
